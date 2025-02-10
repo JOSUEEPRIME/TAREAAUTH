1b6ac2aadc94151b44d13717b3a87b84e1668e68
MEDINA ZUÑIGA NILO JOSUE <nmedinaz@uteq.edu.ec>
2025-02-10 17:45:17 -0500

    Tikotiko

---
+++ app/src/main/java/com/example/xddd/GitHubActivity.java
@@ -0,0 +1,131 @@
+package com.example.xddd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OAuthProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitHubActivity extends AppCompatActivity {
+
+
+    EditText inputEmail;
+    Button btnLogin;
+    FirebaseAuth mAuth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_git_hub);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        btnLogin=findViewById(R.id.btnLogin);
+        mAuth=FirebaseAuth.getInstance();
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString();
+                if (email.matches(emailPattern)) {
+                    Toast.makeText(GitHubActivity.this, "Enter a proper Email", Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    OAuthProvider.Builder provider = OAuthProvider.newBuilder("github.com");
+                    // Target specific email with login hint.
+                    provider.addCustomParameter("login", email);
+
+                    List<String> scopes =
+                            new ArrayList<String>() {
+                                {
+                                    add("user:email");
+                                }
+                            };
+                    provider.setScopes(scopes);
+
+
+                    Task<AuthResult> pendingResultTask = mAuth.getPendingAuthResult();
+                    if (pendingResultTask != null) {
+                        // There's something already here! Finish the sign-in for your user.
+                        pendingResultTask
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<AuthResult>() {
+                                            @Override
+                                            public void onSuccess(AuthResult authResult) {
+                                                // User is signed in.
+                                                // IdP data available in
+                                                // authResult.getAdditionalUserInfo().getProfile().
+                                                // The OAuth access token can also be retrieved:
+                                                // ((OAuthCredential)authResult.getCredential()).getAccessToken().
+                                                // The OAuth secret can be retrieved by calling:
+                                                // ((OAuthCredential)authResult.getCredential()).getSecret().
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(GitHubActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                    } else {
+                        mAuth
+                                .startActivityForSignInWithProvider(/* activity= */ GitHubActivity.this, provider.build())
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<AuthResult>() {
+                                            @Override
+                                            public void onSuccess(AuthResult authResult) {
+                                               openNextActivity();
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(GitHubActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                    }
+                }
+
+
+            }
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void openNextActivity() {
+        Intent intent=new Intent(GitHubActivity.this,HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+}
--- app/src/main/java/com/example/xddd/MainActivity.java
+++ app/src/main/java/com/example/xddd/MainActivity.java
@@ -1,14 +1,94 @@
 package com.example.xddd;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView createnewAccount;
+
+    EditText inputEmail, inputPassword;
+    Button btnLogin;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    ProgressDialog progressDialog;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    ImageView imgGitHub;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createnewAccount = findViewById(R.id.createNewAccount);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword=findViewById(R.id.inputPassword);
+        imgGitHub=findViewById(R.id.imgGithub);
+
+        progressDialog=new ProgressDialog(this);
+        mAuth=FirebaseAuth.getInstance();
+        mUser=mAuth.getCurrentUser();
+
+        createnewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+
+                btnLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v){ perforLogin() ;}
+                });
+
+                imgGitHub.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(MainActivity.this,GitHubActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+
+
+
+
+            }
+        });
+
+
+        FirebaseAuth mAuth;
+        FirebaseAuth mUser;
+        ImageView imgGitHub;
     }
+
+    private void perforLogin() {
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if (!email.matches(emailPattern));
+        {
+            inputEmail.setError("Enter Connext Email");
+
+        }if (password.isEmpty() || password.length() < 6)
+        {
+            inputPassword.setError("Enter Proper Password");
+    }
+}
 }
